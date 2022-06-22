9d69f24a7dc7d8ae56548c60715d02bfc8c158ed
PandaEatingAMuffin <106914794+PandaEatingAMuffin@users.noreply.github.com>
2022-06-22 13:02:15 +0200

    weg!

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-
-const selectElement = document.querySelector('.ice-cream');
-
-selectElement.addEventListener('change', (event) => {
-  const result = document.querySelector('.result');
-  result.textContent = `You like ${event.target.value}`;
-});
-
 window.addEventListener('load', (event) => {
   alert('Je ziel wordt hierbij in beslag genomen door de almachtige Keppelupu')
 })
