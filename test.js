57101d2244cc4c7d2ce428a9f8c9b821e29b71c2
PandaEatingAMuffin <106914794+PandaEatingAMuffin@users.noreply.github.com>
2022-06-22 12:56:28 +0200

    2

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ window.addEventListener('load', (event) => {
   alert('Je ziel wordt hierbij in beslag genomen door de almachtige Keppelupu')
 })
 
-const selectElement = document.querySelector('.bigcheck');
-selectElement.addEventListener('change', (event) => {
+const selectElement2 = document.querySelector('.bigcheck');
+selectElement2.addEventListener('change', (event) => {
   alert('Keppelupu is trots op je voortgang')
 })
