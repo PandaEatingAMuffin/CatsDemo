2d474f1b39ebd87525722a1ea09d8cdf0e386a4c
PandaEatingAMuffin <106914794+PandaEatingAMuffin@users.noreply.github.com>
2022-06-22 12:40:27 +0200

    k

--- test.js
+++ test.js
@@ -5,3 +5,7 @@ selectElement.addEventListener('change', (event) => {
   const result = document.querySelector('.result');
   result.textContent = `You like ${event.target.value}`;
 });
+
+window.addEventListener('load', (event) => {
+  alert('Je ziel wordt hierbij in beslag genomen door de almachtige Keppelupu')
+})
