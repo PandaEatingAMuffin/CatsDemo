b3fb6d8846a855970bd9fcba993e162dfc31d132
PandaEatingAMuffin <106914794+PandaEatingAMuffin@users.noreply.github.com>
2022-06-24 09:25:10 +0200

    labels

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-window.addEventListener('load', (event) => {
-  alert('Je ziel wordt hierbij in beslag genomen door de almachtige Keppelupu')
-})
 
 const selectElement2 = document.querySelector('.bigcheck');
 selectElement2.addEventListener('change', (event) => {
