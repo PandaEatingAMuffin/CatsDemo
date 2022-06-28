341a1b9f413fd15eb1090a19c8a5e20d2b1ab673
PandaEatingAMuffin <106914794+PandaEatingAMuffin@users.noreply.github.com>
2022-06-28 09:07:52 +0200

    test

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ const selectElement2 = document.querySelector('.four');
 selectElement2.addEventListener('change', (event) => {
   alert('Kasper is trots op je voortgang')
 })
+
+const checked = document.querySelector('.accept:checked') !== null;
+console.log(checked);
